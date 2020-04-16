a06e50b7adf9269d80e824e87d52b3a0f418bb0f
Florizotto <florie.mesple@gmail.com>
2020-04-16 13:33:25 +0200

    waterfall plotting fix

--- scampy/plotting.py
+++ scampy/plotting.py
@@ -1,15 +1,15 @@
 def waterfallPlot(ax, x_indices, yz_data, offset):
     zf = len(x_indices)
     for z, z_data in enumerate(yz_data):
-        z_offset = (zf - z) * offset
+        z_offset = z * offset
         ax.plot(x_indices, z_data + z_offset, "k", zorder=(z + 1) * 2)
         # White filling under the curves
-        ax.fill_between(
-            x_indices,
-            z_data + z_offset,
-            offset,
-            facecolor="w",
-            lw=0,
-            zorder=(z + 1) * 2 - 1,
-        )
+#        ax.fill_between(
+#            x_indices,
+#            z_data + z_offset,
+#            offset,
+#            facecolor="w",
+#            lw=0,
+#            zorder=(z + 1) * 2 - 1,
+#        )
     ax.set_xlim([x_indices[0], x_indices[-1]])
